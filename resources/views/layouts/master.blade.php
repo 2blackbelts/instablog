64f9d6b8f405665518f5e1cf32d381545f31349c
Saim McIntosh <s.mcintosh00@gmail.com>
2016-04-20 23:12:51 +0930

    Instablog from PC changing master.blade.php Signed-off-by: Saim McIntosh <s.mcintosh00@gmail.com>

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
 
       <div class="blog-header">
         <h1 class="blog-title">Instablog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <p class="lead blog-description">The official template of creating a Bootstrap Blog.</p>
       </div>
 
       <div class="row">

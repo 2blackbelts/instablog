86d03533ae308c9f27fe01c580444b39dd778611
2blackbelts <s.mcintosh00@gmail.com>
2016-04-20 22:34:27 +0930

    Modified welcome.blade.php to test github pull and fetch
    Signed-off-by: 2blackbelts <s.mcintosh00@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Instablog</div>
                 <p>Hello</p>
             </div>
         </div>
